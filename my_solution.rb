2bc154b9e2383b62a031121c5cde17d4ceb3082d
DeuterGraves <puckles@gmail.com>
2018-09-12 16:40:29 +0100

    oops, completed a couple without commmiting.

--- my_solution.rb
+++ my_solution.rb
@@ -1,15 +1,34 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 # Add "Edinburgh Waverley" to the end of the array
-stops.push("Edinburgh Waverley")
-p stops
+
+# stops.push("Edinburgh Waverley")
+# p stops
+
 # Add "Glasgow Queen St" to the start of the array
-stops.unshift("Glasgow Queen St")
-p stops
+
+# stops.unshift("Glasgow Queen St")
+# p stops
+
 # Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+
+# stops.insert(4, "Polmont")
+# p stops
+
 # Work out the index position of "Linlithgow"
+
+# p stops.index("Linlithgow")
+
 # Remove "Livingston" from the array using its name
+
+# stops.delete("Livingston")
+# p stops
+
 # Delete "Cumbernauld" from the array by index
+
+stops.delete_at(1)
+p stops
+
 # How many stops there are in the array?
 # How many ways can we return "Falkirk High" from the array?
 # Reverse the positions of the stops in the array
